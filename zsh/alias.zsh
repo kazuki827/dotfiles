3b8ab880cd93b6a29c6c53d073f9896dba6e574c
kazuki827 <xm.kazuki827@gmail.com>
2021-08-12 12:02:15 +0900

    edit zsh/alias.zsh

---
+++ zsh/alias.zsh
@@ -0,0 +1,71 @@
+################## git ##################
+# basic
+alias g='git'
+# version
+alias gv='git version'
+# config
+alias gconf='git config --list'
+alias gconfe='git config --global --edit'
+# init 
+alias gi='git init'
+# remote
+alias gr='git remote -v'
+# status
+alias gs='git status'
+alias gss='git status -s'
+# branch
+alias gb='git branch'
+alias gba='git branch -a'
+alias gbm='git branch -m'
+alias gbd='git branch -D'
+# checkout
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gco.='git checkout .' # ワークツリーにある全ファイルの変更を取り消す
+alias gcom='git checkout master'
+alias gcod='git checkout develop'
+# grep
+alias gg='git grep'
+# add
+alias ga='git add'
+alias gaa='git add -A'
+# commit
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gca='git commit --amend'
+# diff
+alias gd='git diff'
+alias gdc='git diff --cached'
+#log
+alias gl='git log'
+alias glo='git log --oneline'
+alias glg='git log --graph --name-status --pretty=format:"%C(red)%h %C(green)%an %C(Cyan)%ad %Creset%s %C(yellow)%d%Creset"'
+# fetch
+alias gfu='git fetch upstream'7
+alias gfo='git fetch origin'
+# merge
+alias gm='git merge'
+alias gma='git merge --abort'
+alias gmom='git merge origin/master'
+alias gmod='git merge origin/develop'
+alias gmud='git merge upstream/develop'
+# push
+alias gp='git push'
+alias gpo='git push origin'
+alias gpom='git push origin master'
+# stash
+alias gst='git stash'
+alias gsl='git stash list'
+alias gsu='git stash -u'
+alias gsa='git stash apply'
+alias gsp='git stash pop'
+# reset
+alias grh='git reset HEAD' #ステージングにある全ファイルをワークツリーに戻す（ git add -A を取り消す）
+
+
+################## vim ##################
+alias v='vim'
+alias vi='vim'
+
+alias mss='mysql.server start'
+alias so='source'
