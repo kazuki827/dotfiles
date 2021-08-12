825fc90bdd452eaed992e01858d75ceddda48501
kazuki827 <xm.kazuki827@gmail.com>
2021-08-12 09:05:26 +0900

    edit deploy.zsh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
+# https://qiita.com/pugiemonn/items/e5fb508df690a323ee14
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .tmux.conf)
+# dotfiles=(.zshrc .tmux.conf)
 
 # .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
 # ホームディレクトリ直下に作成する
-for file in "${dotfiles[@]}"; do
-        ln -svf $file ~/
-done
+# for file in "${dotfiles[@]}"; do
+#         ln -svf $file ~/
+# done
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
