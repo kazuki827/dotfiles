d6e0eaa576d05cabdbc9021bfacca7e979412161
kazuki827 <xm.kazuki827@gmail.com>
2021-08-12 09:26:56 +0900

    edit plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -7,9 +7,39 @@ if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
         print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
 
+
 source "$HOME/.zinit/bin/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 ### End of Zinit's installer chunk
 
+
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+
+# コマンドに対してシンタックスハイライトをしてくれるプラグイン
+zinit light zdharma/fast-syntax-highlighting
+
+
+# 入力中のコマンドと一致するコマンドを履歴から探してサジェストしてくれるプラグイン
+zinit light zsh-users/zsh-autosuggestions
+# サジェストを受け入れるためのキーバインドを設定
+bindkey '^j' autosuggest-accept
+
+
+# git open で現在のgitレポジトリをブラウザで開いてくれるプラグイン
+zinit light paulirish/git-open
+
+
+# bat (見やすいcat)
+zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit light sharkdp/bat
+# ice: 次の行に対してオプションを付与する
+# as"program": zshのソースファイルではなく,コマンドとして扱う
+# from"gh-r": githubのリリースからダウンロードする
+# mv: ダウンロードしたものをリネームする
+# pick: pathを通すものの指定
+# 以下はただのエイリアス設定
+if builtin command -v bat > /dev/null; then
+  alias cat="bat"
+fi
