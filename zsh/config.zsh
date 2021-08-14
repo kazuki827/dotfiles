e076e3e85f3355d8c45dc28b47558de104d20a0d
kazuki827 <xm.kazuki827@gmail.com>
2021-08-14 18:31:59 +0900

    edit zsh/alias.zsh zsh/config.zsh iterm2/

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,9 +2,10 @@
 HISTFILE=$HOME/.zsh-history # 履歴を保存するファイル
 HISTSIZE=100000             # メモリ上に保存する履歴のサイズ
 SAVEHIST=1000000            # 上述のファイルに保存する履歴のサイズ
-setopt inc_append_history   # 実行時に履歴をファイルにに追加していく
-setopt share_history        # 履歴を他のシェルとリアルタイム共有する
+setopt inc_append_history   # 実行時に履歴をファイルにに追加
+setopt share_history        # 履歴を他のシェルとリアルタイム共有
 setopt hist_ignore_dups     # 直前の重複を記録しない
+setopt hist_ignore_all_dups # 入力したコマンドがすでにコマンド履歴に含まれる場合、古い方のコマンドを削除
 setopt hist_reduce_blanks   # historyに保存するときに余分なスペースを削除
 setopt hist_ignore_space    # スペースから始まるコマンド行はヒストリに残さない
 
@@ -34,3 +35,5 @@ setopt nolistbeep       # ビープ音の停止(補完時)
 setopt no_flow_control  # disable ctrl+s, ctrl+q
 setopt print_eight_bit  # 日本語ファイルを扱えるようにする
 setopt correct          # コマンドミスを修正
+setopt auto_param_keys  # カッコの対応などを自動的に補完する
+
