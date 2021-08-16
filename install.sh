9ea85e38d4df88e1d5df3853216eacec5d2339b8
kazuki827 <xm.kazuki827@gmail.com>
2021-08-17 08:55:18 +0900

    create .tmux.conf

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # 未定義な変数があったら途中で終了する
 # set -u
