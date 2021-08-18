7867b70cc1a7efc2b596e4fc208c88bbba0f87e9
kazuki827 <xm.kazuki827@gmail.com>
2021-08-18 21:56:31 +0900

    update

--- .zsh/neta.zsh
+++ .zsh/neta.zsh
@@ -1,13 +1,8 @@
-# [ コマンドを間違えるたびに美少女に罵られたい！ ]
-# https://qiita.com/onokatio/items/4930e4b23339edd05d47
-
-# [ zshでtypoした時に好きな画像を出したい！ ]
-# https://qiita.com/itigoppo/items/a4e68f72ff2ec2a1c883
 function command_not_found_handler()
 {
   if [ -e /usr/local/bin/imgcat ];then
-    if [ -e ~/dotfiles/zsh/img/command_not_found.jpg ];then
-      imgcat ~/dotfiles/zsh/img/command_not_found.jpg
+    if [ -e ~/dotfiles/.zsh/img/command_not_found.jpg ];then
+      imgcat ~/dotfiles/.zsh/img/command_not_found.jpg
     fi
   fi
   echo "$1？ 何言ってんの？"
@@ -16,8 +11,8 @@ function command_not_found_handler()
 function tukareta()
 {
   if [ -e /usr/local/bin/imgcat ];then
-    if [ -e ~/dotfiles/zsh/img/tukareta.gif ];then
-      imgcat ~/dotfiles/zsh/img/tukareta.gif
+    if [ -e ~/dotfiles/.zsh/img/tukareta.gif ];then
+      imgcat ~/dotfiles/.zsh/img/tukareta.gif
     fi
   fi
   echo "お疲れ様！"
