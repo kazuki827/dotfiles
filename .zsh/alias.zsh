31e42faa03b48f099fc3a8ca7480976cb9b855f8
kazuki827 <xm.kazuki827@gmail.com>
2021-09-04 17:45:47 +0900

    update

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -61,6 +61,7 @@ alias gaa='git add -A'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gca='git commit --amend'
+alias gcane='git commit --amend --no-edit'  # ステージングした変更点を直前のコミットに含める。
 
 # diff
 alias gd='git diff'
