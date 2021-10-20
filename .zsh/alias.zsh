9148e886ea7ab70fdb7b6ba3424749e66f59d6fb
kazuki827 <xm.kazuki827@gmail.com>
2021-10-20 16:44:12 +0900

    update

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -252,8 +252,10 @@ alias com='composer'
 ##  Laravel
 ## ----------------------------------------
 alias pa='php artisan'
+alias pammo='php artisan make:model'  # モデル名はテーブル名の単数形にする
+alias pamc='php artisan make:controller'  # モデル名はテーブル名の単数形にする
 alias pamm='php artisan make:migration'
-alias pami='php artisan make:migration'
+alias pami='php artisan migrate'
 
 
 ## ----------------------------------------
