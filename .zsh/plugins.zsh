a2501ed85fbd2d86beea5d355f493771248790c6
Kazuki Matsuda <kazukimatsuda@Kazukis-MacBook-Pro.local>
2021-11-28 16:05:58 +0900

    update

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,17 +1,25 @@
-## ----------------------------------------
-##  Zinit's installer
-## ----------------------------------------
-if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
-        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
 fi
 
-source "$HOME/.zinit/bin/zinit.zsh"
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+### End of Zinit's installer chunk
 
 
 ## ----------------------------------------
@@ -35,7 +43,8 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ffff00"
 ##  fast-syntax-highlighting
 ## ----------------------------------------
 zinit ice wait lucid
-zinit light zdharma/fast-syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
+
 
 
 ## ----------------------------------------
@@ -43,14 +52,8 @@ zinit light zdharma/fast-syntax-highlighting
 ## ----------------------------------------
 # Ctrl+r でコマンド履歴を検索
 zinit ice wait lucid
-zinit light zdharma/history-search-multi-word
+zinit light zdharma-continuum/history-search-multi-word
 
-
-## ----------------------------------------
-##  k
-## ----------------------------------------
-zinit ice wait lucid
-zinit light supercrabtree/k
 
 
 ## ----------------------------------------
