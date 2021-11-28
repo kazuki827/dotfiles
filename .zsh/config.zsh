3f4b0ebe98acbdfb7dacd109df69ab09006fc7ed
kazuki827 <xm.kazuki827@gmail.com>
2021-11-28 20:55:30 +0900

    update

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -6,11 +6,7 @@ typeset -U path cdpath fpath manpath
 ##  Path
 ## ----------------------------------------
 # source ~/opt/anaconda3/etc/profile.d/conda.sh
-# PATH を $PATH:$NODEBREW_DIR/current/bin のように書くと、MacにプリインストールされているNode.jsが優先されることがあるらしい。
-export NODEBREW_DIR="${HOME}/.nodebrew"
-if [ -d "${NODEBREW_DIR}" ]; then
-  export PATH=$NODEBREW_DIR/current/bin:$PATH
-fi
+export PATH=$HOME/.nodebrew/current/bin:$PATH
 
 
 ## ----------------------------------------
